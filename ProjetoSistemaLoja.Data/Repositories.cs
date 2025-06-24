319bc5f91794d645c9589bbaae264f2c0b2ca4d5
Maurício Bono Nunes <67113136+Mauricio-bono@users.noreply.github.com>
2025-06-24 14:00:10 -0300

    Update Repositories.cs

--- ProjetoSistemaLoja.Data/Repositories.cs
+++ ProjetoSistemaLoja.Data/Repositories.cs
@@ -1,16 +1,20 @@
-public class Repositories
+namespace ProjetoSistemaLoja.Data
 {
-    public Repositories(int isArray){
-        if(isArray == 1){
-            UsuarioRepositoryArray usuarioRepository = new UsuarioRepositoryArray();
-            ProdutoRepositoryArray produtoRepository = new ProdutoRepositoryArray();
-            FornecedorRepositoryArray fornecedorRepository = new FornecedorRepositoryArray();
-            TransportadoraRepositoryArray transportadoraRepository = new TransportadoraRepositoryArray();
-        }
-        else{
-            UsuarioRepositoryList usuarioRepository = new UsuarioRepositoryList();
-            ProdutoRepositoryList produtoRepository = new ProdutoRepositoryList();
-            FornecedorRepositoryList fornecedorRepository = new FornecedorRepositoryList();
-            TransportadoraRepositoryList transportadoraRepository = new TransportadoraRepositoryList();
+    public class Repositories
+    {
+        public Repositories(int isArray){
+            if(isArray == 1){
+                UsuarioRepositoryArray usuarioRepository = new UsuarioRepositoryArray();
+                ProdutoRepositoryArray produtoRepository = new ProdutoRepositoryArray();
+                FornecedorRepositoryArray fornecedorRepository = new FornecedorRepositoryArray();
+                TransportadoraRepositoryArray transportadoraRepository = new TransportadoraRepositoryArray();
+            }
+            else{
+                UsuarioRepositoryList usuarioRepository = new UsuarioRepositoryList();
+                ProdutoRepositoryList produtoRepository = new ProdutoRepositoryList();
+                FornecedorRepositoryList fornecedorRepository = new FornecedorRepositoryList();
+                TransportadoraRepositoryList transportadoraRepository = new TransportadoraRepositoryList();
+            }
         }
     }
+}
